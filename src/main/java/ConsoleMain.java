9470698a6bb2ed90a7fbfb242cbcb869b83f5ec4
junyoungpark92 <junyoung.park@meshkorea.net>
2019-04-22 12:49:40 +0900

    init poroject

---
+++ src/main/java/ConsoleMain.java
@@ -0,0 +1,5 @@
+public class ConsoleMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/TempTest.java
@@ -0,0 +1,11 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class TempTest {
+
+    @Test
+    public void testSomething() {
+        assertThat("test").isEqualTo("test");
+    }
+}
